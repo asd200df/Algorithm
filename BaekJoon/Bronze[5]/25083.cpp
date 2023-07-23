8a82e67dadfe4d3f21160369bbb718e4073eff7f
asd200df <109517068+asd200df@users.noreply.github.com>
2023-07-24 00:14:11 +0900

    PS (BOJ 1)
    BOJ (25083)

---
+++ BaekJoon/Bronze[5]/25083.cpp
@@ -0,0 +1,39 @@
+// »õ½Ï (25083) Bronze[5]
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<unordered_map>
+
+#define FastIO (ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL))
+
+using namespace std;
+
+// Function
+void solve();
+
+// Main
+int main(int argc, char** argv) {
+	FastIO;
+
+	solve();
+
+	return 0;
+}
+
+// solution
+void solve() {
+
+	cout << "         ,r'\"7\n" 
+		<< "r`-_   ,'  ,/\n"
+		<< " \\. \". L_r'\n" 
+		<< "   `~\\\/\n" 
+		<< "      |\n" 
+		<< "      |\n";
+
+}
